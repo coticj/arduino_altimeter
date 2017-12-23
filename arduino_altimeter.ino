0aaf7eb73939a748851b281ba5247bdb6cc03afa
Jure Cotic <jure.cotic@celtra.com>
2017-12-23 08:57:35 +0100

    just the alti

--- arduino_altimeter.ino
+++ arduino_altimeter.ino
@@ -1,23 +1,12 @@
-#define PIN 4   
-
-#include <WiFi.h>
-#include <WiFiClient.h>
-#include <WebServer.h>
-#include <ESPmDNS.h>
+#define PIN 4
 #include <Wire.h>
-#include <SPIFFS.h>
-
-#define PIN 4   
 #include <SPI.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP280.h>
-
-
-
-
 #include <Adafruit_NeoPixel.h>
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(4, PIN, NEO_GRB + NEO_KHZ800);           
+Adafruit_BMP280 bmp; // I2C
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(4, PIN, NEO_GRB + NEO_KHZ800);
 int aboveCheck = 0;
 int aboveBreak = 0;
 double baseline, alti;
@@ -27,74 +16,34 @@ int checkAlti = 300; // we passed 300, signal
 int breakAlti = 1550; // breakoff, signal
 int openAlti = 1000; // we are open, stop signaling
 
-extern const uint8_t gamma8[];
-
 //colors
 uint32_t red = strip.Color(255, 0, 0);
 uint32_t green = strip.Color(0, 255, 0);
 uint32_t blue = strip.Color(0, 0, 255);
 uint32_t white = strip.Color(127, 127, 127);
-uint32_t orange = strip.Color(pgm_read_byte(&gamma8[255]), pgm_read_byte(&gamma8[165]), pgm_read_byte(&gamma8[0]));
-
-
-WebServer server(80);
-Adafruit_BMP280 bmp; // I2C
-
-//initialize color correction
-extern const uint8_t gamma8[];
-
-// The value will quickly become too large for an int to store
-unsigned long previousMillis = 0;        // will store last time LED was updated
-
-// constants won't change:
-const long interval = 1000;           // interval at which to blink (milliseconds)
-
-#define DBG_OUTPUT_PORT Serial
-
-const char* ssid = "alti-jure";
-const char* password = "altialti";
-const char* host = "alti1";
 
 void setup()
 {
-  DBG_OUTPUT_PORT.begin(115200);
-  DBG_OUTPUT_PORT.print("\n");
-  DBG_OUTPUT_PORT.setDebugOutput(true);
- 
-  WiFi.softAP(ssid, password);
+  Serial.begin(9600);
 
-  IPAddress myIP = WiFi.softAPIP();
-  Serial.print("AP IP address: ");
-  Serial.println(myIP);
-  
-
-  
-  bmp.begin();
-  httpServer();
-  
-  DBG_OUTPUT_PORT.println("HTTP server started");
-
+  if (!bmp.begin()) {
+    Serial.println(F("Could not find a valid BMP280 sensor, check wiring!"));
+    while (1);
+  }
   baseline=getBaseline();
   Serial.println(baseline);
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
   flashStrip(green, 2,500); // Green, signal ready: 3 times 100ms delay
-  
 }
 
 void loop() {
-
-  unsigned long currentMillis = millis();
-
-  if (currentMillis - previousMillis >= interval) {
-    // save the last time you blinked the LED
-    previousMillis = currentMillis;
-    alti=bmp.readAltitude(baseline*100);
+    alti=bmp.readAltitude(baseline);
     Serial.print(F("Curr alt = "));
     Serial.print(alti); //
     Serial.println(" m");
     Serial.println();
-    
+
     if (alti > checkAlti && aboveCheck == 0) {
       aboveCheck = 1;
       flashStrip(blue,3,200); //we passed 300m, flash
@@ -112,41 +61,53 @@ void loop() {
     if (alti < openAlti && aboveBreak == 1) {
       aboveBreak = 0;
     }
-    
+
+    delay(1000);
+}
+
+// set a reference pressure, smooth it and use this as 0m
+double getBaseline(){
+  double bs;
+  int numReadings = 10;
+
+  double readings[numReadings];
+  int readIndex = 0;
+  double total = 0;
+  double average = 0;
+  int thisReading = 0;
+
+  for (thisReading; thisReading < numReadings; thisReading++) {
+    readings[thisReading] = 0;
   }
-      
-   server.handleClient();
+
+  readIndex = 0;
+  thisReading = 0;
+  //make 10 measurments and then return the average pressure
+  for (thisReading; thisReading < numReadings; thisReading++) {
+
+    readings[thisReading]=bmp.readPressure();
+    total = total + readings[readIndex];
+    readIndex = readIndex + 1;
+    delay(100);
+  }
+  average = total / numReadings;
+  bs=average/100;
+
+  return bs;
 }
 
-float getBatteryVoltage()
-{
-  float measuredvbat = analogRead(A13);
-  Serial.print(measuredvbat);
-  Serial.print(" = ");
-
-  measuredvbat *= 2;    // we divided by 2, so multiply back
-  //measuredvbat *= 3.3;  // Multiply by 3.3V, our reference voltage
-  measuredvbat /= 1024; // convert to voltage
-
-  return measuredvbat;
+void flashStrip(uint32_t color, int numTimes, int duration){
+   for (int i = 0; i < numTimes; i++){
+    fullColor(color);
+    delay(duration);
+    fullColor(strip.Color(0, 0, 0));
+    delay(duration);
+   }
 }
 
-float getBatteryPercentage()
-{
-  return _min(map(getBatteryVoltage() * 10, 3.35 * 10, 4.5 * 10, 0, 100), 100); // Calculate Battery Level (Percent)
-}
-
-float getTemperature()
-{
- return bmp.readTemperature();
-}
-
-float getPressure()
-{
- return bmp.readPressure();
-}
-
-float getAltitude(float baseline)
-{
-  return bmp.readAltitude(baseline);
+void fullColor(uint32_t c) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+  }
 }
